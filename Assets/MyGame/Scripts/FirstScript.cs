e4036052e35af1a4df11f93f9543f126a6db7cc5
smeerws <hello@smeerws.at>
2019-03-27 14:59:07 +0100

    added canvas, text and tmp object; added script, first debug,  change text from script

---
+++ Assets/MyGame/Scripts/FirstScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class FirstScript : MonoBehaviour {
+
+    public Text myText;
+    public TextMeshProUGUI myTmpText;
+
+	// Use this for initialization
+	void Start () {
+        myText.text = "smeerws";
+        myTmpText.text = "hello tmp text";
+
+        Debug.Log("Hello World");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
